75d7da0207b759db9c0352fd78ff2a6932871ad7
Manoel Rios <manoelrios45@gmail.com>
2026-06-26 12:25:31 -0300

    midias

--- components/Chamada.tsx
+++ components/Chamada.tsx
@@ -49,7 +49,7 @@ export default function Chamada({
                 Identificação
               </p>
 
-              <p className="text-[clamp(2.2rem,6vw,5rem)] font-black text-white leading-none">
+              <p className="text-[clamp(5rem,10vw,10rem)] font-black text-white leading-none">
                 {matricula}
               </p>
             </div>
